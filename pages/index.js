f98b02b25df9b87edc0ed33e188b08f64c34cc75
Aleksandir <aleksandir@me.com>
2023-04-01 11:04:46 +1030

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </main>
 
       <Footer />
