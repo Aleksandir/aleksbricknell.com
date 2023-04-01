e2766ff79cd11e10106bdabdb702b935596ca3f6
Aleksandir <aleksandir@me.com>
2023-04-01 14:35:18 +1030

    pushing video

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,11 @@ export default function Home() {
         <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </main>
 
+      <video width="320" height="240" controls>
+        <source src="rickroll.mp4" type="video/mp4"></source>
+        Your browser does not support the video tag.
+      </video>
+
       <Footer />
     </div >
   )
