612f1ff54d1ac34007f9fcc424e0b6428cb532c3
Aleksandir <aleksandir@me.com>
2023-04-01 14:39:10 +1030

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <video width="560" height="315" controls autoplay>
+          <source src="rickroll.mp4" type="video/mp4"></source>
+          Your browser does not support the video tag.
+        </video>
       </main>
 
-      <video width="320" height="240" controls>
-        <source src="rickroll.mp4" type="video/mp4"></source>
-        Your browser does not support the video tag.
-      </video>
 
       <Footer />
     </div >
