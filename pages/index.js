69ba3fdde2ea44a31039ea894876757640911587
Aleksandir <aleksandir@me.com>
2023-04-01 10:58:35 +1030

    removing default code

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Aleks Bricknell</title>
+        <link rel="icon" href="/sackboy.ico" />
       </Head>
 
       <main>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
